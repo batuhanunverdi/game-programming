837adefc7b078db2d6ac26b8eba62621eebd0ab0
Mert Batuhan ÜNVERDİ <mert.batuhan.unverdi@saritay.com>
2022-11-14 22:13:50 +0300

    Hud Added

---
+++ Game Project/GameProject/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <=0)
+        {
+            transform.GetComponent<Animator>().SetTrigger("Die");
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.GetComponent<Animator>().SetTrigger("Attack");
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+
+    }
+}
