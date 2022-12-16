3ec0c49235d9d6439657f82d71748ba264b8025a
Laufeyzn <hakanzavrak2000@gmail.com>
2022-12-16 05:39:02 +0300

    Sound and Teleport Bug Fixed

--- Game Project/GameProject/Assets/Scripts/PlayerAttack.cs
+++ Game Project/GameProject/Assets/Scripts/PlayerAttack.cs
@@ -112,11 +112,11 @@ public class PlayerAttack : MonoBehaviour
             }
             if (other.gameObject == GameObject.FindGameObjectWithTag("TeleportHome"))
             { 
-                StartCoroutine("Teleport", new Vector3(73.1f, 22.03f, 34.92f));
+                StartCoroutine("Teleport", new Vector3(73.1f, 24.03f, 34.92f));
             }
             if (other.gameObject == GameObject.FindGameObjectWithTag("TeleportHomeDesert"))
             {
-                StartCoroutine("Teleport", new Vector3(73.1f, 22.03f, 34.92f));
+                StartCoroutine("Teleport", new Vector3(73.1f, 24.03f, 34.92f));
             }
             /*StartCoroutine("Teleport",new Vector3(6.72f, 2.17f, -2294.65f));
             
