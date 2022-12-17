d4a64cd43d65e9993c8e9bdb491b3f098c4a7064
defnebulut <d.bulut0907@gmail.com>
2022-12-17 22:29:49 +0300

    Added gold info to shop

--- Game Project/GameProject/Assets/EditCharacter/Instantiate.cs
+++ Game Project/GameProject/Assets/EditCharacter/Instantiate.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Instantiate : MonoBehaviour
 {
+    public TMP_Text gold;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +27,8 @@ public class Instantiate : MonoBehaviour
         Statics.PrefabName += "/root/pelvis/spine_01/spine_02/spine_03/";
         GameObject.Find(Statics.PrefabName +"clavicle_l/upperarm_l/lowerarm_l/hand_l/weapon_l/"+PFLogin.shield).SetActive(true);
         GameObject.Find(Statics.PrefabName +"clavicle_r/upperarm_r/lowerarm_r/hand_r/weapon_r/"+PFLogin.weapon).SetActive(true);
+
+        gold.text = PFLogin.gold;
     }
 
     // Update is called once per frame
